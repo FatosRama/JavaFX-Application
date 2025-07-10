6dd066f2f2b89a503ac8e8bb504ced3da2bf3dd1
Fatos Rama <fatosrama.fr@gmail.com>
2025-07-10 17:03:36 +0200

    Created a test Application on the LoginWindow package

--- src/LoginWindow/MainLogInWindow.java
+++ src/LoginWindow/MainLogInWindow.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
+import javafx.scene.control.Label;
 
 
 
@@ -14,9 +15,19 @@ public class MainLogInWindow extends Application{
     @Override
     public void start(Stage stage){
         Pane root = new Pane();
+        Label label1 = new Label("Test message");
+        label1.setLayoutX(50);
+        label1.setLayoutY(50);
+        root.getChildren().add(label1);
+
         Scene scene = new Scene(root, 400, 500);
+        stage.setTitle("Log-In Page");
+        stage.setScene(scene);
+        stage.show();
 
-
+    }
+    public static void main(String[] args){
+        launch();
     }
 
 
