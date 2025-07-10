a00f8eda44bacb905b9bb9c1a67604cd12a29910
Fatos Rama <fatosrama.fr@gmail.com>
2025-07-10 16:54:26 +0200

    Testing commit

---
+++ src/LoginWindow/MainLogInWindow.java
@@ -0,0 +1,24 @@
+package LoginWindow;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+
+
+
+
+
+public class MainLogInWindow extends Application{
+
+    @Override
+    public void start(Stage stage){
+        Pane root = new Pane();
+        Scene scene = new Scene(root, 400, 500);
+
+    }
+
+
+
+
+}
