b782f45a510b96451b030c2f42fd348aa1db4828
Fatos Rama <fatosrama.fr@gmail.com>
2025-07-11 03:32:25 +0200

    Fixed the background color, added panel for Log In

--- src/LoginWindow/MainLogInWindow.java
+++ src/LoginWindow/MainLogInWindow.java
@@ -9,7 +9,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.Button;
 import javafx.scene.shape.Line;
 import javafx.scene.paint.Color;
-
+import javafx.scene.shape.Rectangle;
 
 
 
@@ -20,7 +20,7 @@ public class MainLogInWindow extends Application{
     public void start(Stage stage){
         Pane root = new Pane();
         stage.initStyle(StageStyle.UNDECORATED);
-        root.setStyle("-fx-background-color: black;");
+        root.setStyle("-fx-background-color: #121212;");
 
         Color neonGreen = Color.web("#39ff14");
 
@@ -40,7 +40,15 @@ public class MainLogInWindow extends Application{
 
         titleApplication.setStyle("-fx-font-family:'Tahoma'; -fx-font-size:30px;"+"-fx-text-fill: #39ff14;");
 
+        Label PanelTitle = new Label("JOIN US");
+        PanelTitle.setStyle("-fx-text-fill: white; -fx-font-size:20px;");
+        PanelTitle.setLayoutX(410);
+        PanelTitle.setLayoutY(90);
 
+        Rectangle logInPanel = new Rectangle(300, 450);
+        logInPanel.setFill(Color.web("#1a1a1a"));
+        logInPanel.setLayoutX(300);
+        logInPanel.setLayoutY(70);
 
 
 
@@ -71,14 +79,14 @@ public class MainLogInWindow extends Application{
         Button exitButton = new Button("X");
         exitButton.setLayoutX(859);
         exitButton.setLayoutY(1.5);
-        exitButton.setStyle("-fx-background-color: black;"+"-fx-text-fill: #39ff14");
+        exitButton.setStyle("-fx-background-color: #121212;"+"-fx-text-fill: #39ff14");
 
         exitButton.setPrefSize(40,19);
         exitButton.setOnMouseEntered(e->{
             exitButton.setStyle("-fx-background-color: red;"+"-fx-text-fill : white;");
         });
         exitButton.setOnMouseExited(e->{
-            exitButton.setStyle("-fx-background-color: black;"+"-fx-text-fill: #39ff14");
+            exitButton.setStyle("-fx-background-color: #121212;"+"-fx-text-fill: #39ff14");
         });
 
         exitButton.setOnAction(e -> {
@@ -86,7 +94,7 @@ public class MainLogInWindow extends Application{
         });
 
 
-        root.getChildren().addAll(titleApplication,dLine1,dLine2,dLine3,right,bottom,left,top,exitButton);
+        root.getChildren().addAll(logInPanel,PanelTitle,titleApplication,dLine1,dLine2,dLine3,right,bottom,left,top,exitButton);
         Scene scene = new Scene(root, 900, 600,Color.BLACK);
         stage.setTitle("Log-In Page");
         stage.setScene(scene);
